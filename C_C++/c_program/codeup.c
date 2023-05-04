d406c79cea045db04f91926bd20b99f869e96fc2
jhoh0315 <jhoh0315@gamil.com>
2023-05-04 16:14:14 +0900

    update

--- C_C++/c_program/codeup.c
+++ C_C++/c_program/codeup.c
@@ -1,14 +1,22 @@
-//코드번호 1902
+//코드번호 1953
 
 #include <stdio.h>
 
-int rere(int n){
-  printf("%d\n",n);
-  if(n>1) rere(n-1);
+int d[201];
+
+void drawstar(int n){
+  printf("*");
+  if(n != 1) drawstar(n-1);
+}
+
+void rere(int n){
+  if(n!=1) rere(n-1);
+  drawstar(n);
+  printf("\n");
 }
 
 int main(){
-  int n;
-  scanf("%d",&n);
-  rere(n);
+  int a;
+  scanf("%d",&a);
+  rere(a);
 }
